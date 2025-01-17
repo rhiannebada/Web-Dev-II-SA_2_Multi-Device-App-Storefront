57a40656703f97897c4554bfd95669353243eafb
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2025-01-17 19:19:01 +0400

    Update RelatedProducts.jsx

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -3,18 +3,19 @@ import './RelatedProducts.css'
 import data_product from '../Assets/data'
 import Item from '../Item/Item'
 
-const RelatedProducts = () => {
+// RelatedProducts functional component
+const RelatedProducts = () => { // Main container for the Related Products section
   return (
     <div className='relatedproducts'>
         <h1>Related Products</h1>
         <hr/>
         <div className="relatedproducts-item">
             {data_product.map((item, i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/> // Rendering an Item component for each product, passing necessary props
             })}
         </div>
     </div>
   )
 }
 
-export default RelatedProducts
+export default RelatedProducts // Exporting the RelatedProducts component for use in other parts of the application
