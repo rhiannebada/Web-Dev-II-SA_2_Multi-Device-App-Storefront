61aecd3a9562ae63a9688b10b25a05bb87ee8ab6
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2025-01-17 19:18:32 +0400

    Update Product.jsx

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -5,11 +5,12 @@ import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
+// Product component to display detailed information about a specific product
 const Product = () => {
-    const {all_product} = useContext(ShopContext);
-    const {productId} = useParams();
-    const product = all_product.find((e) => e.id === Number(productId));
-    return (
+    const {all_product} = useContext(ShopContext); // Extracting all_product data from the ShopContext
+    const {productId} = useParams(); // Retrieving the productId parameter from the URL
+    const product = all_product.find((e) => e.id === Number(productId)); // Finding the product in the all_product array that matches the productId
+    return ( // Order of components: ProductDisplay, DescriptionBox, RelatedProducts
         <div>
             <ProductDisplay product={product}/>
             <DescriptionBox />
@@ -18,4 +19,4 @@ const Product = () => {
     )
 };
 
-export default Product;
+export default Product; // exporting the Product component for use in other parts of the application
