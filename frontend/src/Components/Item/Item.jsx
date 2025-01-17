e0883ff3dbdbebd3f83954f62f2cf170f4e882d0
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2025-01-17 19:19:47 +0400

    Update Item.jsx

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './Item.css'
 import { Link } from'react-router-dom';
 
-const Item = (props) => {
+const Item = (props) => { // Define the Item functional component with props to pass data dynamically
   return (
-    <div className='item'>
-      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt={props.name} /></Link>
+    <div className='item'> {/* Main container for the individual item */}
+      <Link to={`/product/${props.id}`}> {/* Link to navigate to the product details page based on the item's ID */}
+        <img onClick={window.scrollTo(0,0)} src={props.image} alt={props.name} /> {/* Display the item image and scroll to the top of the page when clicked */}
+      </Link> 
       <p>{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">
@@ -19,4 +21,4 @@ const Item = (props) => {
   )
 }
 
-export default Item
+export default Item // exporting the Item component for use in other parts of the application
